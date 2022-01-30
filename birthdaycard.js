d001cf973e7c04bf5de0f2d42e98e2dc7d55bf63
jadonjchwong <98238431+jadonjchwong@users.noreply.github.com>
2022-01-30 16:30:12 -0500

    Add files via upload

---
+++ birthdaycard.js
@@ -0,0 +1,9 @@
+function openUp(){
+  const $opencover = document.querySelector('#cover');
+  $opencover.classList.add('animate');
+}
+
+function closeDown(){
+  const $opencover= document.querySelector('#cover');
+  $opencover.classList.remove('animate');
+}
